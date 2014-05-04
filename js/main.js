109c395eeab39dd2f219459800f869bee2cf0457
dsofer <d@sofer.com>
2014-05-04 20:04:12 +0000

    instagram

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-//nothing here yet
+jQuery(function($) {
+    $('.instagram').on('willLoadInstagram', function(event, options) {
+	console.log(options);
+    });
+    $('.instagram').on('didLoadInstagram', function(event, response) {
+	console.log(response);
+    });
+    $('.instagram').instagram({
+	hash: 'love',
+	clientId: 'cbaf79de77bf45b7a2994d6b5ec0b3a7'
+    });
+});
