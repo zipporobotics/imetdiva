d5bd827c9c2b450c7e62933d34d71247178f8cf7
dsofer <d@sofer.com>
2014-05-04 21:15:29 +0000

    edits

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
+
+function createPhotoElement(photo) {
+    var innerHtml = $('<img>')
+        .addClass('instagram-image')
+        .attr('src', photo.images.thumbnail.url);
+
+    innerHtml = $('<a>')
+        .attr('target', '_blank')
+        .attr('href', photo.link)
+        .append(innerHtml);
+
+    return $('<div>')
+        .addClass('instagram-placeholder')
+        .attr('id', photo.id)
+        .append(innerHtml);
+}
+
+function displayPhotos(event, response) {
+    var that = this;
+    $(that).empty();
+    $.each(response.data, function(i, photo) {
+	$(that).append(createPhotoElement(photo));
+    });
+}
+
 jQuery(function($) {
     $('.instagram').on('willLoadInstagram', function(event, options) {
 	console.log(options);
     });
-    $('.instagram').on('didLoadInstagram', function(event, response) {
-	console.log(response);
-    });
+
+    $('.instagram').on('didLoadInstagram', displayPhotos);
+
     $('.instagram').instagram({
 	hash: 'love',
 	clientId: 'cbaf79de77bf45b7a2994d6b5ec0b3a7'
